13edd361ee239c8a63e4d6940617c3f9f80844b8
Silverdust0915 <silverdustgaming@gmail.com>
2025-07-14 12:23:04 +0800

    Add files via upload

---
+++ 2963870.lua
@@ -0,0 +1,13 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 2963870 Manifest
+-- Name: Metal Slug: Awakening
+-- Generated: 2025-07-13 14:20:12
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2963870) -- Metal Slug: Awakening
+
+-- MAIN APP DEPOTS
+addappid(2963871, 1, "d9d37ba599e18f1a9f535214896754232ecaebbb0438ff8516aa3d8eae599d8e") -- Depot 2963871
+setManifestid(2963871, "8121222162422956748", 0)

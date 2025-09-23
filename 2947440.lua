ce3f6b2f1d17e63431bd8d0fe7b44a05dea039b2
Silverdust0915 <silverdustgaming@gmail.com>
2025-09-23 19:37:47 +0800

    Add files via upload

---
+++ 2947440.lua
@@ -0,0 +1,19 @@
+-- 2947440's Lua and Manifest
+-- SILENT HILL f
+-- Generated: 2025-09-23 12:58:05
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2947440) -- SILENT HILL f
+-- MAIN APP DEPOTS
+addappid(2947441, 1, "5329bd39507637997739c33a4b5737f9539ee434a7b79bd875b5fc7d1337c684") -- Depot 2947441
+setManifestid(2947441, "3801962566949899148", 47036702953)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3516200) -- SILENT HILL f - Digital Deluxe Upgrade
